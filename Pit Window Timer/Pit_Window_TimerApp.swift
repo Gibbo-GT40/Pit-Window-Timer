01c8c655e652b9e86ea99e3d07561571be43ffbd
Anthony Gibson <anthony.gibson@icloud.com>
2023-03-24 19:55:57 +0000

    Initial Commit

---
+++ Pit Window Timer/Pit_Window_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pit_Window_TimerApp.swift
+//  Pit Window Timer
+//
+//  Created by Anthony Gibson on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pit_Window_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
